64b73bb71ab64f5e88761e03b2dcec1a0978e4df
= <Khoanguyen0109.99@gmail.com>
2021-02-09 23:24:04 +0700

    first commit

---
+++ theme/src/app/App.js
@@ -0,0 +1,57 @@
+import MomentUtils from '@date-io/moment';
+import '@fake-db';
+import FuseAuthorization from '@fuse/core/FuseAuthorization';
+import FuseLayout from '@fuse/core/FuseLayout';
+import FuseTheme from '@fuse/core/FuseTheme';
+import history from '@history';
+import { createGenerateClassName, jssPreset, StylesProvider } from '@material-ui/core/styles';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { create } from 'jss';
+import jssExtend from 'jss-plugin-extend';
+import rtl from 'jss-rtl';
+import React from 'react';
+import Provider from 'react-redux/es/components/Provider';
+import { Router } from 'react-router-dom';
+import AppContext from './AppContext';
+import { Auth } from './auth';
+import routes from './fuse-configs/routesConfig';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const jss = create({
+	...jssPreset(),
+	plugins: [...jssPreset().plugins, jssExtend(), rtl()],
+	insertionPoint: document.getElementById('jss-insertion-point')
+});
+
+const generateClassName = createGenerateClassName();
+
+const App = () => {
+	return (
+		<AppContext.Provider
+			value={{
+				routes
+			}}
+		>
+			<StylesProvider jss={jss} generateClassName={generateClassName}>
+				<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<MuiPickersUtilsProvider utils={MomentUtils}>
+						<Auth>
+							<Router history={history}>
+								<FuseAuthorization>
+									<FuseTheme>
+										<FuseLayout />
+									</FuseTheme>
+								</FuseAuthorization>
+							</Router>
+						</Auth>
+					</MuiPickersUtilsProvider>
+					</PersistGate>
+				</Provider>
+			</StylesProvider>
+		</AppContext.Provider>
+	);
+};
+
+export default App;
